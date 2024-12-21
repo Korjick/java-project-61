1477f348190bd5d567d24fc1a711da0ac3e9e2ff
Bulat <zzgriim@gmail.com>
2024-12-21 15:46:02 +0100

    Added checkstyle

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,8 +2,8 @@ package hexlet.code;
 
 import java.util.Scanner;
 
-public class cli {
-    public static void input(){
+public class Cli {
+    public static void input() {
         Scanner in = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
