15a315dcb1bdbb40c584c31497bd117b46117629
Bulat <zzgriim@gmail.com>
2025-01-23 03:08:54 +0100

    Fix

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -12,32 +12,32 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        if (args.length > 0) {
-            GameType gameType = GameType.values()[Integer.parseInt(args[0])];
-            if (gameType == GameType.Exit) {
-                return;
-            }
-            String userName = (args.length > 1) ? args[1] : Cli.welcomeUser(in);
-            startGame(gameType, in, userName);
-        } else {
-            GameType gameType = Cli.askGameType(in);
-            if (gameType == GameType.Exit) {
-                return;
-            }
-            String userName = Cli.welcomeUser(in);
-            startGame(gameType, in, userName);
+        GameType gameType = Cli.askGameType(in);
+        if (gameType == GameType.Exit) {
+            return;
         }
-    }
-
-    private static void startGame(GameType gameType, Scanner in, String userName) {
+        String userName = "";
         switch (gameType) {
-            case Even -> EvenGameStrategy.execute(in, userName);
-            case Calc -> CalcGameStrategy.execute(in, userName);
-            case GCD -> GcdGameStrategy.execute(in, userName);
-            case Progression -> ProgressionGameStrategy.execute(in, userName);
-            case Prime -> PrimeGameStrategy.execute(in, userName);
-            default -> {
-            }
+            case Greet:
+                userName = Cli.welcomeUser(in);
+                break;
+            case Even:
+                EvenGameStrategy.execute(in, userName);
+                break;
+            case Calc:
+                CalcGameStrategy.execute(in, userName);
+                break;
+            case GCD:
+                GcdGameStrategy.execute(in, userName);
+                break;
+            case Progression:
+                ProgressionGameStrategy.execute(in, userName);
+                break;
+            case Prime:
+                PrimeGameStrategy.execute(in, userName);
+                break;
+            default:
+                break;
         }
     }
 }
